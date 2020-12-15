3d1b070b5dcec6b0af29c2c20f49b9c364833168
Annie Seth <60269506+annieseth@users.noreply.github.com>
2020-12-15 17:41:23 -0500

    try 2

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>SARANG AND ANNIE ARE MAKING AN APP!</Text>
       <StatusBar style="auto" />
     </View>
   );
