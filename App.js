829a76f46d9065bf8bb695607b3d017e12893b2a
sarang-pujari <sarangpujari01@gmail.com>
2020-12-18 15:56:06 -0500

    sarang sex

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>sexy time</Text>
+      <Text>sarang is sx7</Text>
       <StatusBar style="auto" />
     </View>
   );
