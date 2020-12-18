8fbbe734529a926ad449527ffe0627643433407c
Annie Seth <60269506+annieseth@users.noreply.github.com>
2020-12-18 15:54:16 -0500

    annie tried

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>I AM NOW PUSHING THE PULL!!!!!!!! SARANG AND ANNIE ARE MAKING AN APP!</Text>
+      <Text>sexy time</Text>
       <StatusBar style="auto" />
     </View>
   );
