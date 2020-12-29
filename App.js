78b59bd2473db29bfe1c63b96831bb74cd3bd25f
sarang-pujari <sarangpujari01@gmail.com>
2020-12-29 15:49:55 -0500

    end of 2020 edits

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>sarang is sx7</Text>
+      <Text>sarang is sx243</Text>
       <StatusBar style="auto" />
     </View>
   );
